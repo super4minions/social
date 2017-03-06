9b5b5a60095f9a205ce43a805f95117051c44822
SupervisionT <a.alshaikh82@gmail.com>
2017-03-06 13:01:13 -0800

    retrieve the data from the http respons and list the data in the profile

--- view/profile.js
+++ view/profile.js
@@ -1,67 +1,67 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//     var span = document.createElement("SPAN");
+//     var txt = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(txt);
+//     myNodelist[i].appendChild(span);
+// }
+//
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function() {
+//         var div = this.parentElement;
+//         div.style.display = "none";
+//     }
+// }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+// // Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//     if (ev.target.tagName === 'LI') {
+//         ev.target.classList.toggle('checked');
+//     }
+// }, false);
 
 // Create a new list item when clicking on the "Add" button
   var http = new XMLHttpRequest();
-function newElement() {
-  console.log("rquest");
+  function newElement() {
   var inputValue = document.getElementById("myInput").value;
   http.open("POST", "/posts", true);
   http.send(inputValue);
 
-            var li = document.createElement("li");
             if (inputValue === '') {
                 alert("You must write something!");
             } else {
               http.onreadystatechange = function(response) {
                   if (http.readyState == 4 && http.status == 200) {
                       jsonOptions = JSON.parse(http.responseText);
-                      console.log("jsonOptions", jsonOptions);
-                      var t = document.createTextNode(jsonOptions);
-                      li.appendChild(t);
-                      document.getElementById("myUL").appendChild(li);
-              }
-            }
+                      jsonOptions.reverse().forEach(function(elem){
+                        var li = document.createElement("li");
+                        var t = document.createTextNode(elem.post_contents);
+                        li.appendChild(t);
+                        document.getElementById("myUL").appendChild(li);
+                      });
+                    }
+                }
             }
             document.getElementById("myInput").value = "";
 
-            var span = document.createElement("SPAN");
-            var txt = document.createTextNode("\u00D7");
-            span.className = "close";
-            span.appendChild(txt);
-            li.appendChild(span);
-
-            for (i = 0; i < close.length; i++) {
-                close[i].onclick = function() {
-                    var div = this.parentElement;
-                    div.style.display = "none";
-                }
-            }
+            // var span = document.createElement("SPAN");
+            // var txt = document.createTextNode("\u00D7");
+            // span.className = "close";
+            // span.appendChild(txt);
+            // li.appendChild(span);
+            //
+            // for (i = 0; i < close.length; i++) {
+            //     close[i].onclick = function() {
+            //         var div = this.parentElement;
+            //         div.style.display = "none";
+            //     }
+            // }
 
 }
