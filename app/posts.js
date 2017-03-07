75ef9a16be63602c490c4bae3db47330da249e9a
4daliaomry4 <alaakhattab92@gmail.com>
2017-03-07 13:54:00 +0200

    add heroku link in request

--- app/posts.js
+++ app/posts.js
@@ -9,7 +9,7 @@ var config = {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
-module.exports = function(req, res) {
+module.exports = function (req, res) {
     utils.parseBody(req, function(err, data) {
         dbutils.insertToPostsTable(data,config, function(err) {
 dbutils.selectFromDB("posts",config,function(err,result){
