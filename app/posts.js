1eeab204fb69cf4bccf0cd33c7a10b1b0b7dd9f3
4daliaomry4 <alaakhattab92@gmail.com>
2017-03-09 10:39:10 +0200

    test

--- app/posts.js
+++ app/posts.js
@@ -4,6 +4,7 @@ var client = dbutils.dbconnection;
 
 module.exports = function(req, res) {
     utils.parseBody(req, function(err, data) {
+      console.log("req.headers.cookie",req.headers.cookie);
         var ck = req.headers.cookie.split(/=([^;]*)/);
         var token = ck[ck.indexOf("token")+1];
         dbutils.insertToPostsTable(Object.keys(data)[0], token, client, function(err) {
